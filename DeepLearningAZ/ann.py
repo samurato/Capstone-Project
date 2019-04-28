6b0ae61a765b8198c81274a9257b8f476d5e96ac
Samrat Pant <samrat.sammie@gmail.com>
2019-04-29 01:23:04 +1000

    ANN with Ann tuning and grid search

--- DeepLearningAZ/ann.py
+++ DeepLearningAZ/ann.py
@@ -49,21 +49,28 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
+
+#-*--------------------------
+#
+
 #Part 2 :- Making an ANN, Import Keras,  builds Ann with #Tensorflow backend
 import keras
 from keras.models import Sequential 
 from keras.layers import Dense
+from keras.layers import Dropout
+
 
 #Initialising the ANN to perform classification
 classifier= Sequential()
 
-#Add Input layer and first hidden layer
+#Add Input layer and first hidden layer with dropouts
 classifier.add(Dense(units=6, kernel_initializer="uniform", activation = 'relu', input_dim = 11))
-
+classifier.add(Dropout(p = 0.1))
 classifier.add(Dense(units=6, kernel_initializer="uniform", activation = 'relu'))
-
+classifier.add(Dropout(p=0.1))
 ##Output layer
-classifier.add(Dense(units=1, kernel_initializer="uniform", activation = 'sigmoid'))
+classifier.add(Dense(units=1, kernel_initializer="uniform", activation ='sigmoid'))
 
 #Compile ANN :-Applying Stocastic Gradinet Decent
 classifier.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
@@ -75,6 +82,96 @@ classifier.fit(X_train, y_train, batch_size=10, nb_epoch = 100)
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
+
+#Use our ANN model to predict if the customer with the following informations will leave the bank: 
+#
+#Geography: France
+#Credit Score: 600
+#Gender: Male
+#Age: 40 years old
+#Tenure: 3 years
+#Balance: $60000
+#Number of Products: 2
+#Does this customer have a credit card ? Yes
+#Is this customer an Active Member: Yes
+#Estimated Salary: $50000
+#So should we say goodbye to that customer ?
+#
+#The solution is provided in the next Lecture but I strongly recommend that you try to solve it on your own.
+
+
+homework_pred = classifier.predict(sc.transform(np.array([[0.0,0,600,1,40,3,60000,2,1,1,50000]])))
+homework_pred = (homework_pred > 0.5)
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+#---------------------------------
+
+#Tuning and Evlauating the ANN using sci-kit learn
+#careful crossvalidation
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+from keras.models import Sequential 
+from keras.layers import Dense
+def build_classifier():
+   classifier= Sequential()
+   classifier.add(Dense(units=6,kernel_initializer="uniform", activation = 'relu', input_dim = 11))
+   classifier.add(Dense(units=6, kernel_initializer="uniform", activation = 'relu'))
+   classifier.add(Dense(units=1, kernel_initializer="uniform", activation ='sigmoid'))
+   classifier.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+   return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, nb_epoch = 100)
+#in windows PC if it is not working use this 
+if __name__ == "__main__":
+    #Execute accuracies only if the Multicore processing is supported
+    accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs= -1 )
+
+
+
+
+
+
+
+
+
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
